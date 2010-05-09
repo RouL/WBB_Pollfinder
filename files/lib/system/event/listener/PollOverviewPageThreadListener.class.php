bd4852e0eb24221aad89844937eb83b278eb2a1a
RouL <roul@codingcorner.info>
2010-05-09 16:36:20 +0000

    WBBFPIT-1

--- files/lib/system/event/listener/PollOverviewPageThreadListener.class.php
+++ files/lib/system/event/listener/PollOverviewPageThreadListener.class.php
@@ -1,8 +1,6 @@
 <?php
 // wcf imports
 require_once(WCF_DIR.'lib/system/event/EventListener.class.php');
-// wbb imports
-require_once(WBB_DIR.'lib/data/thread/Thread.class.php');
 
 /**
  * Checks for posts with a poll and sets the pollID of the first post with a poll.
@@ -20,11 +18,25 @@ class PollOverviewPageThreadListener implements EventListener {
 	 * @see EventListener::execute()
 	 */
 	public function execute($eventObj, $className, $eventName) {
-		if (isset($_REQUEST['threadID'])) {
+		if (isset($_REQUEST['threadID']) && intval($_REQUEST['threadID']) != 0) {
 			$threadID = intval($_REQUEST['threadID']);
-			$thread = new Thread($threadID);
 			
+			require_once(WBB_DIR.'lib/data/thread/Thread.class.php');
+			$thread = new Thread($threadID);
+			if (!$thread->threadID) {
+				throw new IllegalLinkException();
+			}
 			$thread->enter();
+			
+			$sql = "SELECT	pollID
+				FROM	wbb".WBB_N."_post
+				WHERE	threadID = ".$threadID."
+					AND	isDeleted = 0
+					AND	isDisabled = 0
+					AND	pollID > 0
+				ORDER BY postID ASC";
+			$row = WCF::getDB()->getFirstRow($sql);
+			$eventObj->pollID = $row['pollID'];
 		}
 	}
 }
